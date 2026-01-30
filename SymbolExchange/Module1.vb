9516152bc12413ce81273b6fe180834973639cc9
Alexander <pchp250g8@tutamail.com>
2026-01-30 16:17:37 +0300

    Добавьте файлы проекта.

---
+++ SymbolExchange/Module1.vb
@@ -0,0 +1,14 @@
+﻿Module Module1
+
+    Sub Main()
+        Console.WriteLine("Input a string")
+        Dim strLine1 = Console.ReadLine()
+        Dim nLen = strLine1.Length
+        Dim strLine2 = strLine1(nLen - 1) & strLine1.Substring(1, nLen - 2) & strLine1(0)
+        Console.WriteLine("Exchange the first and the last symbol in the string")
+        Console.WriteLine("Source string:{0}", strLine1)
+        Console.WriteLine("Destinstion string:{0}", strLine2)
+        Console.Read()
+    End Sub
+
+End Module
---
+++ SymbolExchange/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Общие сведения об этой сборке предоставляются следующим набором
+' набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+' связанные со сборкой.
+
+' Проверьте значения атрибутов сборки
+
+<Assembly: AssemblyTitle("SymbolExchange")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("SymbolExchange")>
+<Assembly: AssemblyCopyright("Copyright ©  2026")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+<Assembly: Guid("e85327db-d2b0-4496-8a31-17ff9702cd36")>
+
+' Сведения о версии сборки состоят из следующих четырех значений:
+'
+'      Основной номер версии
+'      Дополнительный номер версии
+'   Номер сборки
+'      Редакция
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
